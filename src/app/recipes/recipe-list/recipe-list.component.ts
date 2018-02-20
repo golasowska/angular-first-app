a1825d846f6b28beded37d0879a81170e5fd39c7
Anna Golasowska <annagolasowska@gmail.com>
2018-02-20 21:02:44 +0100

    added elements to recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe( 'Spaghetti Carbonara', 'This is simply a test',
+      'https://media4.popsugar-assets.com/files/2016/04/26/094/n/1922398/25749f2795cdc1d1_carbonara3.xxxlarge_2x.jpg'),
+    new Recipe( 'Spaghetti Carbonara', 'This is simply a test',
+      'https://media4.popsugar-assets.com/files/2016/04/26/094/n/1922398/25749f2795cdc1d1_carbonara3.xxxlarge_2x.jpg'),
+    new Recipe( 'Spaghetti Carbonara', 'This is simply a test',
+      'https://media4.popsugar-assets.com/files/2016/04/26/094/n/1922398/25749f2795cdc1d1_carbonara3.xxxlarge_2x.jpg')
+  ];
 
   constructor() { }
 
