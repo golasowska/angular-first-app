45d4265c039c6de1ea1d33ebd1fb38d67c40fc4c
Anna Golasowska <annagolasowska@Mac-mini-Ania.local>
2018-02-20 11:16:09 +0100

    databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+///<reference path="../../node_modules/@angular/http/src/http_module.d.ts"/>
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
