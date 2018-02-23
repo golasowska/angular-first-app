e8222681e3ed01a67ab27fdeeb7d4ee7eb6325b0
golasowska <annagolasowska@gmail.com>
2018-02-23 11:49:35 +0100

    added Child Routing, passing dynamic parameters to links, added event component and  events to edit mode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import {RouterModule, Routes} from '@angular/router';
 
 import {RecipesComponent} from './recipes/recipes.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipesEditComponent} from './recipes/recipes-edit/recipes-edit.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipesComponent },
+  { path: 'recipes', component: RecipesComponent, children: [
+      { path: '', component: RecipeStartComponent },
+      { path: 'new', component RecipesEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component RecipesEditComponent }
+    ] },
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
